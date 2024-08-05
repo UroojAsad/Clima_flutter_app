a66ab9c56d84532fb49b427a79299fba1981cf76
UroojAsad <uroojshehzadi82@gmail.com>
2024-08-05 10:03:35 -0700

    json

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 
 
@@ -15,6 +16,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
  @override
  void initState( ) {
    super.initState();
+   getlocation();
    requestLocationPermission();
 
  }
@@ -44,9 +46,30 @@ class _LoadingScreenState extends State<LoadingScreen> {
   print(location.longitude);
   }
 
+ void getdata() async {
+   final uri = Uri.parse('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289@10d714a6e88630761fae22');
+   http.Response response = await http.get(uri);
+
+   if(response.statusCode==200){
+     String data= response.body;
+
+     var decodedData=jsonDecode(data);
+
+     var temperature = decodedData['main']['temp'];
+     var condition = decodedData ['weather'][0]['id'];
+     var cityName = decodedData['name'];
+     print(temperature);
+     print(condition);
+     print(cityName);
+
+   }else{
+     print(response.statusCode);
+   }
+ }
 
   @override
   Widget build(BuildContext context) {
+   getdata();
     return Scaffold();
   }
 }
